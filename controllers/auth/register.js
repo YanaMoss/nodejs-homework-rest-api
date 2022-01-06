9dc61a2b754ec5e484362797c7fce15df9ac85e8
Yana Sokurenko <yanamoss@gmail.com>
2022-01-06 12:23:55 +0200

    fix

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,6 +1,8 @@
 const { Conflict } = require('http-errors');
 const gravatar = require('gravatar');
+const { uuid } = require('uuid');
 
+const { sendEmail } = require('../../helpers');
 const { User } = require('../../models');
 
 const register = async (req, res) => {
@@ -9,12 +11,19 @@ const register = async (req, res) => {
   if (user) {
     throw new Conflict(`User with ${email} already exist`);
   }
+  const verificationToken = uuid();
   const avatarURL = gravatar.url(email);
-  const newUser = new User({ name, email, avatarURL });
+  const newUser = new User({ name, email, avatarURL, verificationToken });
 
   newUser.setPassword(password);
 
-  newUser.save();
+  await newUser.save();
+  const mail = {
+    to: email,
+    subject: 'Подтверждения email',
+    html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Подтвердить email</a>`,
+  };
+  await sendEmail(mail);
   res.status(201).json({
     status: 'success',
     code: 201,
@@ -23,6 +32,7 @@ const register = async (req, res) => {
         email,
         name,
         avatarURL,
+        verificationToken,
       },
     },
   });
